8ca4f6d33f7c67c30c2af96c0bd7679dfec2386f
Abie <abiesagitarius@gmail.com>
2019-04-17 15:15:07 +0700

    Updates

---
+++ precache-manifest.1466879602cad6589e90fb456954b431.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9198ae3b868f769fc8ea",
+    "url": "/pics/static/js/runtime~main.7218180c.js"
+  },
+  {
+    "revision": "6329cc9a5ba0f2d64563",
+    "url": "/pics/static/js/main.a85c2d8a.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/pics/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ae00358721bce8af0ede19c79b2c95ee",
+    "url": "/pics/index.html"
+  }
+];
