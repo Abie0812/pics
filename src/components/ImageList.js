943bfa56289eeba1b971dddb3823b54202d9597b
Abie <abiesagitarius@gmail.com>
2019-05-19 21:26:52 +0700

    [ADD] Grid CSS

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,13 @@
+import './ImageList.css';
 import React from 'react';
 
-class ImageList extends React.Component {
+const ImageList = props => {
+    console.log(props);
+    const images = props.images.map(({description, id, urls}) => {
+        return <img alt={description} key={id} src={urls.regular} />
+    });
 
-    render();
+    return <div className="image-list">{images}</div>
 };
 
 export default ImageList;
-
